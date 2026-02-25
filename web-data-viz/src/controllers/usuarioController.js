4af373ecda0eab5e7829fd2f504374dabce2f4a4
ak-miyuki <andreia.kubota@sptech.school>
2026-02-24 21:14:33 -0300

    alterei css cadastro, tabela usuarios, model, route e controller do usuario

--- web-data-viz/src/controllers/usuarioController.js
+++ web-data-viz/src/controllers/usuarioController.js
@@ -27,6 +27,7 @@ function autenticar(req, res) {
                                         id: resultadoAutenticar[0].id,
                                         email: resultadoAutenticar[0].email,
                                         nome: resultadoAutenticar[0].nome,
+                                        sobrenome: resultadoAutenticar[0].sobrenome,
                                         senha: resultadoAutenticar[0].senha,
                                         aquarios: resultadoAquarios
                                     });
@@ -54,6 +55,7 @@ function autenticar(req, res) {
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
+    var sobrenome = req.body.sobrenomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
     var fkEmpresa = req.body.idEmpresaVincularServer;
@@ -61,6 +63,8 @@ function cadastrar(req, res) {
     // Faça as validações dos valores
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
+    } else if (sobrenome == undefined) {
+        res.status(400).send("Seu sobrenome está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
@@ -70,7 +74,7 @@ function cadastrar(req, res) {
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, fkEmpresa)
+        usuarioModel.cadastrar(nome, sobrenome, email, senha, fkEmpresa)
             .then(
                 function (resultado) {
                     res.json(resultado);
